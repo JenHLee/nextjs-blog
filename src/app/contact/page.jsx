b573354ee1b6e07d01f232c77e2cc3e06b1d9784
JenHLee <hj.jennie.lee@gmail.com>
2023-07-11 17:13:19 -0600

    add contact styling and animation to home, contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import styles from './page.module.css'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button";
+
+export const metadata = {
+  title: "Jennie Contact Information",
+  description: "This is Contact Page",
+};
 
 const Contact = () => {
   return (
     <div className={styles.container}>
-      Contact
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            name=""
+            id=""
+            placeholder="message"
+            cols="30"
+            rows="10"
+            className={styles.textArea}
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
