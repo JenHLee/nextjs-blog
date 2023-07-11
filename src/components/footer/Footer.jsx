ed18ad0533f5225c3258e98d436d69580c8529e8
JenHLee <hj.jennie.lee@gmail.com>
2023-07-11 14:36:24 -0600

    create project and css styling

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>©2023 jenlog. All rights reserved.</div>
+      {/* <div>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+      </div> */}
+    </div>
+  );
+};
+
+export default Footer;
