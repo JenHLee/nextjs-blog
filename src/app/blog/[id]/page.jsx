19d87b38952685a7d8f3c187f86d1c1cc48dc005
JenHLee <hj.jennie.lee@gmail.com>
2023-07-13 00:21:45 -0600

    add blog and blogpost styling

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const BlogPost = () => {
   return (
-    <div>
-      BlogPost
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>title</h1>
+          <p className={styles.desc}>
+            He was an expert but not in a discipline that anyone could fully
+            appreciate. He knew how to hold the cone just right so that the soft
+            server ice-cream fell into it at the precise angle to form a perfect
+            cone each and every time. It had taken years to perfect and he could
+            now do it without even putting any thought behind it. Nobody seemed
+            to fully understand the beauty of this accomplishment except for the
+            new worker who watched in amazement.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Jennie Lee</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it. Nobody seemed to fully understand the beauty of this accomplishment except for the new worker who watched in amazement. He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it. Nobody seemed to fully understand the beauty of this accomplishment except for the new worker who watched in amazement.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
