0eb668e7ac1d22d3409f823f87590ed0210e72b1
JenHLee <hj.jennie.lee@gmail.com>
2023-07-16 00:44:02 -0600

    add mongodb, connect to db using .env

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,16 +4,15 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", { 
+  const res = await fetch("http://localhost:3000/api/posts", {
     // next: { revalidate: 10 }  // revalidate everytime 10s (cache lifetime of a resource is 10s)
-    next: { cache: 'no-store'  } 
+    next: { cache: "no-store" },
   });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
-  return (
-  res.json());
+  return res.json();
 }
 
 const Blog = async () => {
@@ -22,19 +21,20 @@ const Blog = async () => {
   return (
     <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href="/blog/testId" className={styles.container} key={item.id}>
+        <Link href={item._id} className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
             <Image
-              src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+              src={item.img}
               alt=""
               width={400}
               height={250}
               className={styles.image}
             />
           </div>
+          {item.content}
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>desc</p>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
         </Link>
       ))}
