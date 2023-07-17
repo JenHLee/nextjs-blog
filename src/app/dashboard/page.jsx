e8f324ca4ca3d5a7763f9cec21c27ec2532dc4e6
Hyunju Jennie Lee <97131199+JenHLee@users.noreply.github.com>
2023-07-16 23:51:39 -0600

    delete comment

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -33,8 +33,7 @@ const Dashboard = () => {
   // fetch data in client side
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
-  // console.log(data);
-
+  
   return <div>Dashboard</div>;
 };
 
